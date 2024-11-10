9908456e5a3b7dbd054d7c5bd99cb6b4a3a626de
charz185 <80986803+charz185@users.noreply.github.com>
2024-11-10 16:50:19 -0500

    working

---
+++ Dungeness.cs
@@ -0,0 +1,226 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Drawing;
+using System.Collections;
+using System.Drawing.Printing;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System.Collections.Immutable;
+using System.Data.SqlTypes;
+class Dungeness
+{
+    public Dungeness()
+    {
+        ;
+    }
+    public static List<Color> MakeArrayFromImage(String image)
+    {
+        List<Color> newPixelArray = [];
+        Bitmap img = new Bitmap(image);
+
+        for (int j = 0; j < img.Height; j++)
+        {
+            for (int i = 0; i < img.Width; i++)
+            {
+                newPixelArray.Add(img.GetPixel(i, j));
+            }
+        }
+        return newPixelArray;
+    }
+    public static int[] getBitmapSize(string image)
+    {
+        Bitmap img = new Bitmap(image);
+        int[] sizeOfImg = [img.Width, img.Height];
+        return sizeOfImg;
+    }
+
+    public static List<List<object>> sortList(Dictionary<Color, int> list)
+    {
+
+        int n = list.Count;
+        List<List<object>> array1 = [];
+        for (int i = 0; i < list.Count; i++)
+        {
+            array1.Add(new List<object> { list.Keys.ElementAt(i), list.Values.ElementAt(i) });
+        }
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if ((int)array1[j][1] > (int)array1[min_idx][1])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            List<object> temp = array1[min_idx];
+            array1[min_idx] = array1[i];
+            array1[i] = temp;
+        }
+        return array1;
+    }
+    public static void SaveBmpAsPNG(Bitmap bmp1, string path)
+    {
+        bmp1.Save(path, ImageFormat.Png);
+    }
+
+
+    //Random
+    private static int FindSeedOfBatch(List<Color> UniqueList, List<Color> batch)
+    {
+        ;
+        bool running = true;
+        while (running)
+        {
+            List<int> indexes = [];
+            foreach (Color c in batch)
+            {
+                indexes.Add(UniqueList.IndexOf(c));
+            }
+            int OtherSeed = RandomGen.nextSeed(indexes, UniqueList.Count);
+            Console.WriteLine("Working " + OtherSeed);
+            return OtherSeed;
+        }
+        return -1;
+    }
+    private static void saveToBytes(List<Color> unique, List<int> seeds, String path, int batchSize, int[] imgSize)
+    {
+        using (FileStream fileStream = new FileStream(path, FileMode.Create))
+        {
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+            {
+                binaryWriter.Write((Int16)(batchSize));
+                binaryWriter.Write((byte)imgSize[0]);
+                binaryWriter.Write((byte)imgSize[1]);
+                binaryWriter.Write((Int16)unique.Count);
+                for (int i = 0; i < unique.Count; i++)
+                {
+                    binaryWriter.Write(unique[i].R);
+                    binaryWriter.Write(unique[i].G);
+                    binaryWriter.Write(unique[i].B);
+                    binaryWriter.Write(unique[i].A);
+                }
+                foreach (int i in seeds)
+                {
+                    binaryWriter.Write((Int16)i);
+                }
+                binaryWriter.Close();
+            }
+        }
+    }
+    private static List<object> readFromBin(String path)
+    {
+        int batchSize = 4;
+        int[] imgSize = new int[2];
+        List<Color> unique = [];
+        List<int> seeds = [];
+
+
+        using (FileStream fileStream = new FileStream(path, FileMode.Open))
+        {
+            using (BinaryReader binaryReader = new BinaryReader(fileStream))
+            {
+                batchSize = binaryReader.ReadInt16();
+                imgSize[0] = binaryReader.ReadByte();
+                imgSize[1] = binaryReader.ReadByte();
+                int uniqueCount = binaryReader.ReadInt16();
+                for (int i = 0; i < uniqueCount; i++)
+                {
+                    Byte r = binaryReader.ReadByte();
+                    Byte g = binaryReader.ReadByte();
+                    Byte b = binaryReader.ReadByte();
+                    Byte a = binaryReader.ReadByte();
+
+                    unique.Add(Color.FromArgb(a, r, g, b));
+                }
+                while (binaryReader.BaseStream.Length > binaryReader.BaseStream.Position + 1)
+                {
+                    seeds.Add(binaryReader.ReadUInt16());
+                    Console.WriteLine(seeds.Count);
+                }
+            }
+        }
+        List<object> returnList = [batchSize, imgSize, unique, seeds];
+        return returnList;
+    }
+    public static void ProcCompressImg(String path, String savePath, int batchSize = -1)
+    {
+        List<Color> Old = MakeArrayFromImage(path);
+
+        int[] ImgSize = getBitmapSize(path);
+        Console.WriteLine(Old.Count);
+        if (batchSize == -1)
+        {
+            for (int i = 4; i < 100; i++)
+            {
+                if (Old.Count % i == 0)
+                {
+                    batchSize = i;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("Batch Size " + batchSize);
+        List<Color> OldUnique = (Old.Distinct().ToList());
+        Console.WriteLine(OldUnique.Count);
+        int count = 0;
+        List<int> returnList = [];
+        List<List<Color>> list = new();
+        for (int i = 0; i < Old.Count; i += batchSize)
+        {
+            list.Add(Old.GetRange(i, batchSize));
+        }
+        int z = 0;
+        foreach (List<Color> i in list)
+        {
+            int seedFound = FindSeedOfBatch(OldUnique, i);
+            returnList.Add(seedFound);
+
+            z += batchSize;
+            Console.WriteLine("Z" + z);
+        }
+        // saveList.AddRange(returnList);
+        //File.WriteAllLines("result.txt", saveList);
+        saveToBytes(OldUnique, returnList, savePath, batchSize, ImgSize);
+    }
+    public static void procDecompressImg(string path, string output)
+    {
+        List<object> returns = readFromBin(path);
+        int batchSize = (int)returns[0];
+        int[] imgSize = (int[])returns[1];
+        List<Color> Uniques = (List<Color>)returns[2];
+        Console.WriteLine(Uniques[0]);
+        List<int> seeds = (List<int>)returns[3];
+        List<Color> Pixels = [];
+        for (int i = 0; i < seeds.Count; i++)
+        {
+            Console.WriteLine(seeds[i]);
+            RandomGen rnd = new RandomGen(seeds[i]);
+            List<int> indexes = rnd.nextBatch(Uniques.Count, batchSize);
+            foreach (int x in indexes)
+            {
+                Pixels.Add(Uniques[x]);
+            }
+        }
+        Bitmap newImg = new(imgSize[0], imgSize[1]);
+        int counter = 0;
+        foreach (Color x in Pixels)
+        {
+            int y = (int)(counter / imgSize[0]);
+            newImg.SetPixel(counter % imgSize[0], y, x);
+            counter++;
+        }
+        SaveBmpAsPNG(newImg, output);
+    }
+}
+
---
+++ RandomGen.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Z3;
+using System.ComponentModel.DataAnnotations;
+// https://stackoverflow.com/a/13533895
+class RandomGen
+{
+    private long last;
+    private long inc;
+
+    public RandomGen(long seed)
+    {
+        this.last = seed;
+        this.inc = seed;
+    }
+
+    public int nextInt(int max)
+    {
+        last ^= (last << 23);
+        last ^= (last >>> 35);
+        last ^= (last << 4);
+        int out1 = (int)((last) % max);
+        return (out1 < 0) ? -out1 : out1;
+    }
+    public List<int> nextBatch(int max, int amt) 
+    {
+        List<int> ints = new List<int>();
+        for (int i = 0; i < amt; i++)
+        {
+            ints.Add(nextInt(max));
+        }
+        return ints;
+    }
+
+    public static int nextSeed(List<int> indexes,int max)
+    {
+        int max1 = (int)(Math.Pow(2, 16))-1;
+        for (int i = 1;i < max1; i++)
+        {
+            RandomGen rnd = new RandomGen(i);
+            List<int> new1 = rnd.nextBatch(max,indexes.Count);
+            if (new1.SequenceEqual(indexes)) {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+    public static bool checkIfSeedTrue(int seed, int index, int max)
+    {
+        RandomGen rnd = new RandomGen(seed);
+        if (rnd.nextInt(max) == index) { return true; }
+        return false;
+
+    }
+
+}
+
+
