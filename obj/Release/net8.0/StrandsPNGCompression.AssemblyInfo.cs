1e312c2490e7f6286464975ac29c48bc0916fd72
charz185 <80986803+charz185@users.noreply.github.com>
2024-11-12 13:09:35 -0500

    for testing

--- obj/Release/net8.0/StrandsPNGCompression.AssemblyInfo.cs
+++ obj/Release/net8.0/StrandsPNGCompression.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrandsPNGCompression")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bad051fa00eca10a0369ba5c6ba5bffe1c6de70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9d45a687188245edc41203acc9c0e47d6c468f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StrandsPNGCompression")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StrandsPNGCompression")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
